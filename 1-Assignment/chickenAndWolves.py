643b2dbae47ed77e070bd26b354d5f83fbf179a3
David Sahni <sahnid@oregonstate.edu>
2018-04-20 00:32:54 -0700

    messed with the heuristic numbers and got better results

--- 1-Assignment/chickenAndWolves.py
+++ 1-Assignment/chickenAndWolves.py
@@ -257,12 +257,12 @@ def heuristic(newnode, goalBank, Parent):
         if state.numMoved == 1:
             return 1
         else:
-            return 2
+            return 4
     elif goalBank != Parent.State.boatBank: #we are going to the goal bank, send as many as possible!
         if state.numMoved == 2:
             return 1
         else:
-            return 2
+            return 4
 
 initFile = ""
 goalFile = ""
