c3422bcb97813ac40c150fe6847d1e7a2c287010
David Sahni <sahnid@oregonstate.edu>
2018-06-03 15:45:18 -0700

    added more class stuff

--- 2-Assignment/classFile.py
+++ 2-Assignment/classFile.py
@@ -5,8 +5,31 @@ class Review:
 
 
 class Node:
+    name = ""
     parent = None
-    child = []
-    #P(M|D) ordered [(m=t d=t), (m=t d=f), (m=f d=t), (m=f d=f)]
-    cpt = []
+    childNodes = []
+    #P(M|D) ordered [(m=t d=t), (m=t d=f)]
+    cpt = (0, 0)
+
+    def __init__(self, name, parentNode=None):
+        self.parent = parentNode
+        self.name = name
+
+
+class AI:
+    parentNode = None
+    features = None
+    vocab = None
+
+    def __init__(self, parentNode, features, vocab):
+        self.parentNode = parentNode
+        self.features = features
+        self.vocab = vocab
+
+    def findWord(self, word):
+        return self.vocab.index(word)
+
+    def getNode(self, word):
+        i = self.findWord(word)
+        return self.parentNode.childNodes[i]
 
